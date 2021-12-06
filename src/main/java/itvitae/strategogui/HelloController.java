9dac3afd6179f24459e08a915ffc16baad530024
JasperMooren <jasper.mooren@hotmail.nl>
2021-12-06 11:49:50 +0100

    Initial commit

---
+++ src/main/java/itvitae/strategogui/HelloController.java
@@ -0,0 +1,14 @@
+package itvitae.strategogui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
