f133b18964eb2a89311fec8e48cc0bd78c474720
AlwinTeeuwen <95620171+AlwinTeeuwen@users.noreply.github.com>
2021-12-06 13:54:41 +0100

    typo constant
    Changed name constants cause typo in the word LENGTH

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -4,8 +4,8 @@ public class Board {
 
     // Constants
     public static final int BOARD_SIZE = 10;
-    public static final int X_LENGHT = BOARD_SIZE;
-    public static final int Y_LENGHT = BOARD_SIZE;
+    public static final int X_LENGTH = BOARD_SIZE;
+    public static final int Y_LENGTH = BOARD_SIZE;
 
     // Instance Variables
     private Square[][] squares;
@@ -15,7 +15,7 @@ public class Board {
     }
 
     private Square[][] buildSquares() {
-        Square[][] squares = new Square[Y_LENGHT][X_LENGHT];
+        Square[][] squares = new Square[Y_LENGTH][X_LENGTH];
         for (int y = 0; y < squares.length; y++) {
             for (int x = 0; x < squares[y].length; x++) {
                 squares[y][x] = new Square();
