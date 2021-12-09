bae143aef3d5e0c74e073dc33118f9d00e1c6595
JasperMooren <jasper.mooren@hotmail.nl>
2021-12-09 16:01:29 +0100

    Update Player.java
    End of day commit.
    Changed some positions and added a reset method.

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -64,12 +64,52 @@ public class Player {
         for (int y = 0; y < setupPieces.length; y++) {
             for (int x = 0; x < setupPieces[y].length; x++) {
                 if (!placePiece(x, y, setupPieces[y][x])) {
-                    // TODO: repair stuff
+                    reset(x, y, setupPieces);
                     return false;
                 }
             }
         }
         return true;
+    }
+
+    /**
+     * refills the pieces ArrayList and empties the board, based on incorrect changes to both
+     * @param maxX the x position at which the error occurred
+     * @param maxY the y position at which the error occurred
+     * @param setupPieces the format from which the player tried to add pieces.
+     */
+    private void reset(int maxX, int maxY, Class<? extends AbstractPiece>[][] setupPieces) {
+        for (int y = 0; y < setupPieces.length; y++) {
+            for (int x = 0; x < setupPieces[y].length; x++) {
+                if (x == maxX && y == maxY) {
+                    return;
+                }
+                placePiece(x, y, (AbstractPiece) null);
+                // try always works, because it asks for a constructor of a subclass of AbstractPiece,
+                // which has to be implemented
+                try {
+                    // Reflectively constructs a piece based on the subclass given in the array.
+                    AbstractPiece piece =
+                            setupPieces[x][y].getDeclaredConstructor(Player.class).newInstance(this);
+                    pieces.add(piece);
+                } catch (Exception e) { // this code should never run -> getDeclaredConstructor() demands it.
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * Places a piece on a coordinate based on the orientation of the player.
+     *
+     * @param playerX the x coordinate to place the piece from the perspective of the player
+     * @param playerY the y coordinate to place the piece from the perspective of the player
+     * @param piece   the piece to place.
+     */
+    public void placePiece(int playerX, int playerY, AbstractPiece piece) {
+        int boardX = getFlippedX(playerX);
+        int boardY = getBoardY(playerY);
+        board.setPiece(boardX, boardY, piece);
     }
 
     /**
@@ -86,95 +126,10 @@ public class Player {
         if (abstractPiece == null) {
             return false;
         }
-        int boardX = getBoardX(playerX);
+        int boardX = getFlippedX(playerX);
         int boardY = getBoardY(playerY);
         board.setPiece(boardX, boardY, abstractPiece);
         return true;
-    }
-
-    // Standard Openings with hard coded setups:
-    private Class<? extends AbstractPiece>[][] getDutchOpening() {
-        // all classes are hardcoded subclasses of Piece, therefore this is always allowed.
-        @SuppressWarnings("unchecked")
-        Class<? extends AbstractPiece>[][] setup = new Class[][]{
-                {P2Scout.class, P3Miner.class, PBomb.class, P2Scout.class, P3Miner.class,
-                        PBomb.class, PFlag.class, PBomb.class, P3Miner.class, P3Miner.class},
-                {P4Sergeant.class, PBomb.class, P4Sergeant.class, P7Major.class, P8Colonel.class,
-                        P5Lieutenant.class, PBomb.class, P5Lieutenant.class, P6Captain.class, P4Sergeant.class},
-                {P5Lieutenant.class, P4Sergeant.class, PBomb.class, P1Spy.class, P9General.class,
-                        P2Scout.class, P7Major.class, P7Major.class, P8Colonel.class, P2Scout.class},
-                {P6Captain.class, P2Scout.class, P2Scout.class, P5Lieutenant.class, P2Scout.class,
-                        P6Captain.class, P3Miner.class, P10Marshal.class, P2Scout.class, P6Captain.class}
-        };
-        return setup;
-    }
-
-    private Class<? extends AbstractPiece>[][] getDeBoerOffense() {
-        // all classes are hardcoded subclasses of Piece, therefore this is always allowed.
-        @SuppressWarnings("unchecked")
-        Class<? extends AbstractPiece>[][] setup = new Class[][]{
-                {P7Major.class, P3Miner.class, P3Miner.class, P3Miner.class, P4Sergeant.class,
-                        PBomb.class, PFlag.class, PBomb.class, PBomb.class, P3Miner.class},
-                {P7Major.class, P2Scout.class, P7Major.class, P1Spy.class, P6Captain.class,
-                        P5Lieutenant.class, PBomb.class, P4Sergeant.class, P5Lieutenant.class, P2Scout.class},
-                {P4Sergeant.class, P2Scout.class, P8Colonel.class, P8Colonel.class, P9General.class,
-                        P2Scout.class, P4Sergeant.class, PBomb.class, PBomb.class, P5Lieutenant.class},
-                {P10Marshal.class, P6Captain.class, P5Lieutenant.class, P3Miner.class, P2Scout.class,
-                        P6Captain.class, P2Scout.class, P2Scout.class, P2Scout.class, P6Captain.class}
-        };
-        return setup;
-    }
-
-    private Class<? extends AbstractPiece>[][] getShorelineGambit() {
-        // all classes are hardcoded subclasses of Piece, therefore this is always allowed.
-        @SuppressWarnings("unchecked")
-        Class<? extends AbstractPiece>[][] setup = new Class[][]{
-                {P2Scout.class, P4Sergeant.class, P3Miner.class, P5Lieutenant.class, P2Scout.class,
-                        P1Spy.class, P4Sergeant.class, PBomb.class, P5Lieutenant.class, P3Miner.class},
-                {P5Lieutenant.class, P4Sergeant.class, P6Captain.class, P6Captain.class, P3Miner.class,
-                        P3Miner.class, P6Captain.class, P2Scout.class, P6Captain.class, P5Lieutenant.class},
-                {P7Major.class, P9General.class, P8Colonel.class, PBomb.class, P10Marshal.class,
-                        P7Major.class, PBomb.class, P3Miner.class, P8Colonel.class, P7Major.class},
-                {P2Scout.class, P2Scout.class, PBomb.class, PFlag.class, PBomb.class,
-                        PBomb.class, P4Sergeant.class, P2Scout.class, P2Scout.class, P2Scout.class}
-        };
-        return setup;
-    }
-
-    private Class<? extends AbstractPiece>[][] getFlagFlankAssault() {
-        // all classes are hardcoded subclasses of Piece, therefore this is always allowed.
-        @SuppressWarnings("unchecked")
-        Class<? extends AbstractPiece>[][] setup = new Class[][]{
-                {PFlag.class, PBomb.class, P7Major.class, P3Miner.class, P7Major.class,
-                        P6Captain.class, PBomb.class, P3Miner.class, P2Scout.class, P5Lieutenant.class},
-                {PBomb.class, P3Miner.class, P1Spy.class, P7Major.class, P6Captain.class,
-                        PBomb.class, P3Miner.class, P6Captain.class, P5Lieutenant.class, P2Scout.class},
-                {P8Colonel.class, P10Marshal.class, P2Scout.class, P8Colonel.class, PBomb.class,
-                        P5Lieutenant.class, P2Scout.class, P5Lieutenant.class, P2Scout.class, P3Miner.class},
-                {P6Captain.class, P2Scout.class, P9General.class, PBomb.class, P4Sergeant.class,
-                        P4Sergeant.class, P2Scout.class, P2Scout.class, P4Sergeant.class, P4Sergeant.class}
-        };
-        return setup;
-    }
-
-    private Class<? extends AbstractPiece>[][] getScottishDefense() {
-        // all classes are hardcoded subclasses of Piece, therefore this is always allowed.
-        @SuppressWarnings("unchecked")
-        Class<? extends AbstractPiece>[][] setup = new Class[][]{
-                {PFlag.class, PBomb.class, P4Sergeant.class, PBomb.class, P6Captain.class,
-                        P3Miner.class, P3Miner.class, P3Miner.class, P5Lieutenant.class, P7Major.class},
-                {PBomb.class, P4Sergeant.class, PBomb.class, P2Scout.class, P2Scout.class,
-                        P5Lieutenant.class, P4Sergeant.class, P6Captain.class, P2Scout.class, P6Captain.class},
-                {P4Sergeant.class, PBomb.class, P3Miner.class, P6Captain.class, P7Major.class,
-                        P10Marshal.class, P2Scout.class, P2Scout.class, P7Major.class, P2Scout.class},
-                {PBomb.class, P9General.class, P1Spy.class, P8Colonel.class, P4Sergeant.class,
-                        P4Sergeant.class, P2Scout.class, P2Scout.class, P4Sergeant.class, P4Sergeant.class}
-        };
-        return setup;
-    }
-
-    private Class<? extends AbstractPiece>[][] getMineFieldDefense() {
-
     }
 
     /**
@@ -198,13 +153,18 @@ public class Player {
         return abstractPiece;
     }
 
-    private int getBoardX(int playerX) {
+    /**
+     * If the player is the top player, the board positions should flip.
+     * @param x the original cooridinate
+     * @return the flipped coordinate (from board x to player x or from player x to board x)
+     */
+    private int getFlippedX(int x) {
         if (isFirst) {
-            return playerX;
+            return x;
         }
         // -1 because a board length of 10 goes from 0 to 9, so position 2 flips to 7, not 8.
-        int boardX = Board.X_LENGTH - playerX - 1;
-        return boardX;
+        int flippedX = Board.X_LENGTH - x - 1;
+        return flippedX;
     }
 
 
@@ -216,5 +176,103 @@ public class Player {
         int boardY = Board.Y_LENGTH - playerY - 1;
         ;
         return boardY;
+    }
+
+    // Standard Openings with hard coded setups:
+    private static Class<? extends AbstractPiece>[][] getDutchOpening() {
+        // all classes are hardcoded subclasses of Piece, therefore this is always allowed.
+        @SuppressWarnings("unchecked")
+        Class<? extends AbstractPiece>[][] setup = new Class[][]{
+                {P2Scout.class, P3Miner.class, PBomb.class, P2Scout.class, P3Miner.class,
+                        PBomb.class, PFlag.class, PBomb.class, P3Miner.class, P3Miner.class},
+                {P4Sergeant.class, PBomb.class, P4Sergeant.class, P7Major.class, P8Colonel.class,
+                        P5Lieutenant.class, PBomb.class, P5Lieutenant.class, P6Captain.class, P4Sergeant.class},
+                {P5Lieutenant.class, P4Sergeant.class, PBomb.class, P1Spy.class, P9General.class,
+                        P2Scout.class, P7Major.class, P7Major.class, P8Colonel.class, P2Scout.class},
+                {P6Captain.class, P2Scout.class, P2Scout.class, P5Lieutenant.class, P2Scout.class,
+                        P6Captain.class, P3Miner.class, P10Marshal.class, P2Scout.class, P6Captain.class}
+        };
+        return setup;
+    }
+
+    private static Class<? extends AbstractPiece>[][] getDeBoerOffense() {
+        // all classes are hardcoded subclasses of Piece, therefore this is always allowed.
+        @SuppressWarnings("unchecked")
+        Class<? extends AbstractPiece>[][] setup = new Class[][]{
+                {P7Major.class, P3Miner.class, P3Miner.class, P3Miner.class, P4Sergeant.class,
+                        PBomb.class, PFlag.class, PBomb.class, PBomb.class, P3Miner.class},
+                {P7Major.class, P2Scout.class, P7Major.class, P1Spy.class, P6Captain.class,
+                        P5Lieutenant.class, PBomb.class, P4Sergeant.class, P5Lieutenant.class, P2Scout.class},
+                {P4Sergeant.class, P2Scout.class, P8Colonel.class, P8Colonel.class, P9General.class,
+                        P2Scout.class, P4Sergeant.class, PBomb.class, PBomb.class, P5Lieutenant.class},
+                {P10Marshal.class, P6Captain.class, P5Lieutenant.class, P3Miner.class, P2Scout.class,
+                        P6Captain.class, P2Scout.class, P2Scout.class, P2Scout.class, P6Captain.class}
+        };
+        return setup;
+    }
+
+    private static Class<? extends AbstractPiece>[][] getShorelineGambit() {
+        // all classes are hardcoded subclasses of Piece, therefore this is always allowed.
+        @SuppressWarnings("unchecked")
+        Class<? extends AbstractPiece>[][] setup = new Class[][]{
+                {P2Scout.class, P4Sergeant.class, P3Miner.class, P5Lieutenant.class, P2Scout.class,
+                        P1Spy.class, P4Sergeant.class, PBomb.class, P5Lieutenant.class, P3Miner.class},
+                {P5Lieutenant.class, P4Sergeant.class, P6Captain.class, P6Captain.class, P3Miner.class,
+                        P3Miner.class, P6Captain.class, P2Scout.class, P6Captain.class, P5Lieutenant.class},
+                {P7Major.class, P9General.class, P8Colonel.class, PBomb.class, P10Marshal.class,
+                        P7Major.class, PBomb.class, P3Miner.class, P8Colonel.class, P7Major.class},
+                {P2Scout.class, P2Scout.class, PBomb.class, PFlag.class, PBomb.class,
+                        PBomb.class, P4Sergeant.class, P2Scout.class, P2Scout.class, P2Scout.class}
+        };
+        return setup;
+    }
+
+    private static Class<? extends AbstractPiece>[][] getFlagFlankAssault() {
+        // all classes are hardcoded subclasses of Piece, therefore this is always allowed.
+        @SuppressWarnings("unchecked")
+        Class<? extends AbstractPiece>[][] setup = new Class[][]{
+                {PFlag.class, PBomb.class, P7Major.class, P3Miner.class, P7Major.class,
+                        P6Captain.class, PBomb.class, P3Miner.class, P2Scout.class, P5Lieutenant.class},
+                {PBomb.class, P3Miner.class, P1Spy.class, P7Major.class, P6Captain.class,
+                        PBomb.class, P3Miner.class, P6Captain.class, P5Lieutenant.class, P2Scout.class},
+                {P8Colonel.class, P10Marshal.class, P2Scout.class, P8Colonel.class, PBomb.class,
+                        P5Lieutenant.class, P2Scout.class, P5Lieutenant.class, P2Scout.class, P3Miner.class},
+                {P6Captain.class, P2Scout.class, P9General.class, PBomb.class, P4Sergeant.class,
+                        P4Sergeant.class, P2Scout.class, P2Scout.class, P4Sergeant.class, P4Sergeant.class}
+        };
+        return setup;
+    }
+
+    private static Class<? extends AbstractPiece>[][] getScottishDefense() {
+        // all classes are hardcoded subclasses of Piece, therefore this is always allowed.
+        @SuppressWarnings("unchecked")
+        Class<? extends AbstractPiece>[][] setup = new Class[][]{
+                {PFlag.class, PBomb.class, P4Sergeant.class, PBomb.class, P6Captain.class,
+                        P3Miner.class, P3Miner.class, P3Miner.class, P5Lieutenant.class, P7Major.class},
+                {PBomb.class, P4Sergeant.class, PBomb.class, P2Scout.class, P2Scout.class,
+                        P5Lieutenant.class, P4Sergeant.class, P6Captain.class, P2Scout.class, P6Captain.class},
+                {P4Sergeant.class, PBomb.class, P3Miner.class, P6Captain.class, P7Major.class,
+                        P10Marshal.class, P2Scout.class, P2Scout.class, P7Major.class, P2Scout.class},
+                {PBomb.class, P9General.class, P1Spy.class, P8Colonel.class, P2Scout.class,
+                        P5Lieutenant.class, P3Miner.class, P8Colonel.class, P2Scout.class, P5Lieutenant.class}
+        };
+        return setup;
+    }
+
+    private static Class<? extends AbstractPiece>[][] getMineFieldDefense() {
+        // all classes are hardcoded subclasses of Piece, therefore this is always allowed.
+        @SuppressWarnings("unchecked")
+        Class<? extends AbstractPiece>[][] setup = new Class[][]{
+                {PFlag.class, PBomb.class, P2Scout.class, P2Scout.class, P2Scout.class,
+                        P2Scout.class, P2Scout.class, P2Scout.class, P3Miner.class, P4Sergeant.class},
+                {PBomb.class, P10Marshal.class, P5Lieutenant.class, P3Miner.class, P3Miner.class,
+                        P7Major.class, P6Captain.class, P5Lieutenant.class, P5Lieutenant.class, P5Lieutenant.class},
+                {P9General.class, P8Colonel.class, P1Spy.class, P3Miner.class, P6Captain.class,
+                        P8Colonel.class, P6Captain.class, P6Captain.class, P7Major.class, P4Sergeant.class},
+                {P2Scout.class, P2Scout.class, P3Miner.class, P7Major.class, PBomb.class,
+                        PBomb.class, P4Sergeant.class, P4Sergeant.class, PBomb.class, PBomb.class}
+
+        };
+        return setup;
     }
 }
