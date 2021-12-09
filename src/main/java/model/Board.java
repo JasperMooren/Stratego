ba3b7728523a4d5195e0ecde9c9c473334540b2d
EwouddeGroote <ewouddegroote@gmail.com>
2021-12-09 16:00:20 +0100

    Revert "Update Board.java"
    This reverts commit bcc7046a6b63a04e41e71971c8235e8a9c7b0e3e.

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -1,5 +1,7 @@
 package model;
 
+import model.pieces.*;
+
 public class Board {
 
     // Constants
@@ -12,6 +14,21 @@ public class Board {
 
     public Board() {
         squares = buildSquares();
+        // test setup//
+        setPiece(3,2, new Bomb(new Player(this)));
+        setPiece(2,3, new Bomb(new Player(this)));
+        setPiece(4,3, new Bomb(new Player(this)));
+        setPiece(3,3, new Flag(new Player(this)));
+        setPiece(4,2, new Miner(new Player(this)));
+        setPiece(5,3, new Sergeant(new Player(this)));
+        setPiece(6,2, new Lieutenant(new Player(this)));
+        setPiece(7,3, new Captain(new Player(this)));
+        setPiece(8,2, new Major(new Player(this)));
+        setPiece(9,3, new Colonel(new Player(this)));
+        setPiece(8,1, new General(new Player(this)));
+        setPiece(8,0, new Marshal(new Player(this)));
+        setPiece(9,0, new Spy(new Player(this)));
+        setPiece(3,1, new Scout(new Player(this)));
     }
 
     private Square[][] buildSquares() {
@@ -22,5 +39,9 @@ public class Board {
             }
         }
         return squares;
+    }
+
+    public void setPiece(int x, int y, Piece piece){
+        squares[y][x].setPiece(piece);
     }
 }
