be1616ae58b8be8791d43fc77ca0ed794f7458b2
JasperMooren <jasper.mooren@hotmail.nl>
2022-01-12 15:16:41 +0100

    Fixed typo

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -6,8 +6,8 @@ public class Board {
 
     // Constants
     public static final int BOARD_SIZE = 10;
-    public static final int X_LENGHT = BOARD_SIZE;
-    public static final int Y_LENGHT = BOARD_SIZE;
+    public static final int X_LENGTH = BOARD_SIZE;
+    public static final int Y_LENGTH = BOARD_SIZE;
 
     // Instance Variables
     private final Square[][] squares;
@@ -20,7 +20,7 @@ public class Board {
 
     // constructor methods
     private Square[][] buildSquares() {
-        Square[][] squares = new Square[Y_LENGHT][X_LENGHT];
+        Square[][] squares = new Square[Y_LENGTH][X_LENGTH];
         for (int y = 0; y < squares.length; y++) {
             for (int x = 0; x < squares[y].length; x++) {
                 squares[y][x] = new Square();
